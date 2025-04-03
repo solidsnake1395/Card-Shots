2ba1975b95b083dfe25a58ef15868c6cf59c05a6
solidsnake1395 <aick1995@gmail.com>
2025-04-03 03:07:06 +0200

    Update index.php to retrieve environment variables for APP_ENV and APP_DEBUG

--- public/index.php
+++ public/index.php
@@ -5,7 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$env = getenv('APP_ENV') ?: 'prod';
+$debug = getenv('APP_DEBUG') === '1';
+
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
